6e0404fffbea90f25313ddd70117224b0dc68f21
Dylan <dylanjaxxon@gmail.com>
2013-11-10 23:23:32 -0500

    commiting to give teammate access

--- src/engine/GameEngine.java
+++ src/engine/GameEngine.java
@@ -2,22 +2,24 @@ package engine;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
 import jgame.platform.JGEngine;
 import model.*;
 
 public class GameEngine extends JGEngine {
 	
+	private GameManager myGameManager;
 	private final int myViewerWidth = 800;
 	private final int myViewerHeight = 600;
 	private int myTileWidth;
 	private int myTileHeight;
 	private ArrayList<Unit> myUnits;
+	private ArrayList<Tile> myTiles;
+	private ArrayList<Player> myPlayers;
+	private Player currentPlayer;
 	
-	private Map myMap;
-	
-	public GameEngine() {
+	public GameEngine(GameDataObject gameData) {
 		initEngineComponent(myViewerWidth, myViewerHeight);
+		myGameManager = new GameManager(gameData);
 	}
 	
 	public void initCanvas() {
@@ -25,11 +27,21 @@ public class GameEngine extends JGEngine {
 		
 		
 	}
-
+	
 	public void initGame() {
-		
-		
-		
+		setFrameRate(35, 1);
+		//myGameManager.dfhdhdxj
+		defineAllImages();
+	}
+	
+	public void doFrame() {
+		myGameManager.manageTurns();
+	}
+	
+	public void manageTurns() {
+		if (currentPlayer.getRemainingMoves() == 0) {
+			nextPlayer();
+		}
 	}
 	
 	
--- src/engine/Player.java
+++ src/engine/Player.java
@@ -2,4 +2,9 @@ package engine;
 
 public class Player {
 
+	private int myRemainingMoves = 1;
+	
+	public int getRemainingMoves() {
+		return myRemainingMoves;
+	}
 }
