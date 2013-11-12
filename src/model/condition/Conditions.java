c5f132fabfeca13eb101041813dd8f87472a9c70
JohnGodbey <jrg35@duke.edu>
2013-11-11 23:19:53 -0500

    set conditions as a glorified Collection

--- src/model/condition/Conditions.java
+++ src/model/condition/Conditions.java
@@ -1,9 +1,27 @@
 package model.condition;
 
+import java.util.*;
+
 public class Conditions {
+	private Collection<Condition> myConditions = new ArrayList<Condition>();
 
 	public Conditions() {
-		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean check(){
+		for (Condition c : myConditions){
+			if(!c.check())
+				return false;
+		}
+		return true;
 	}
 
+	public boolean addCondition(Condition c){
+		return myConditions.add(c);
+	}
+	
+	public boolean removeCondition(Condition c){
+		return myConditions.remove(c);
+	}
+	
 }
