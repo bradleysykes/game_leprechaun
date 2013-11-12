8dba935b7da0fee023bf6c989d0544adf467c41c
Brad Sykes <bradleysykes1@gmail.com>
2013-11-11 19:03:58 -0500

    Merge code

--- src/gae/MapView.java
+++ src/gae/MapView.java
@@ -10,20 +10,19 @@ public class MapView extends JGEngine {
 
 	public MapView(){
 		initEngineComponent(500,500);
-		this.addMouseListener(new MapMouseListener(this));
 	}
 	
 	
 	@Override
 	public void initCanvas() {
-		setCanvasSettings(1, 1, displayWidth(), displayHeight(), JGColor.blue, JGColor.blue, null);
+		setCanvasSettings(16, 16, 500, 500, JGColor.blue, JGColor.blue, null);
 		
 	}
 	
 	public void placeOnBoard(){
 		if(this.getKey(256)&&BoardBuffer.retrieve()!=null){
 			defineImage(BoardBuffer.retrieve().getName(),"-",0, "resources/"+BoardBuffer.retrieve().getRelativeImagePath(),"-");
-			new JGObject("test", true, this.getMouseX(),this.getMouseY(), 0, BoardBuffer.retrieve().getName()).snapToGrid();
+			new JGObject("test", true, this.getMouseX(),this.getMouseY(), 0, BoardBuffer.retrieve().getName());
 		}
 	}
 	
@@ -34,7 +33,7 @@ public class MapView extends JGEngine {
 	@Override
 	public void initGame() {
 		setFrameRate(40, 1);
-		setPFSize(16,16);
+		setPFSize(1,1);
 	}
 
 }
