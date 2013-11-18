309a5e56a5f2b10afb11d0495edfb804b2a5c5e7
William Shelburne <wds9@duke.edu>
2013-11-18 00:12:26 -0500

    finished save state implementation

--- src/data/encoder/SaverHandler.java
+++ src/data/encoder/SaverHandler.java
@@ -1,8 +1,15 @@
 package data.encoder;
 
+import data.GameElements;
+
 //Place Holder Class for Save method in gae.controller
 public class SaverHandler {
 	public SaverHandler() {
 		
+	}
+
+	public void doSave(GameElements currentState) {
+		// TODO Auto-generated method stub
+		System.out.println(currentState.getPlayers().size());
 	}
 }
