cac8adb6ca8a2e6d81e8097bd08ab6a5efb5ab8f
Seunghyun Lee <shlee0605@gmail.com>
2013-11-17 22:12:45 -0500

    updated game elements

--- src/data/GameElements.java
+++ src/data/GameElements.java
@@ -7,43 +7,35 @@ import model.GameMap;
 import model.Player;
 import model.unit.Unit;
 
-// maybe return controller??
 
 public class GameElements {
     private GameMap myGameMap;
-    private List<Unit> myUnits;
     private List<Player> myPlayers;
     private Map<String, String> myImages;
 
     public GameElements(GameMap map, List<Unit> units, List<Player> players) {
         myGameMap = map;
-        myUnits = units;
         myPlayers = players;
     }
     
     public GameElements(GameMap map) {
         myGameMap = map;
-        myUnits = new ArrayList<Unit>();
         myPlayers = new ArrayList<Player>();
     }
     
     public GameMap getGameMap() {
         return myGameMap;
     }
-    
-    public List<Unit> getUnits() {
-        return myUnits;
-    }
-    
+       
     public List<Player> getPlayers() {
         return myPlayers;
     }
     
-    public void addUnit(Unit unit) {
-        myUnits.add(unit);
-    }
-    
     public void addPlayer(Player player) {
         myPlayers.add(player);
+    }
+    
+    public Map<String, String> getImageMap() {
+        return myImages;
     }
 }
