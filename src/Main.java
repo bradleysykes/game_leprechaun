14835de0931d620f1993b3bf2bc8790279e011ed
Dylan <dylanjaxxon@gmail.com>
2013-11-17 19:37:31 -0500

    added code to scale images. incomplete

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import util.ImageTool;
 import engine.GameViewer;
 import gae.EditGUI;
 
@@ -7,5 +8,6 @@ public class Main {
 	public static void main(String[] args){
 		new EditGUI();
 		//new GameViewer();
+		//ImageTool.scaleImage("C:\\Users\\Dylan\\cs308\\oogasalad_MISSINGNO\\testimages\\FFV_Barrier_Tower_SNES_BG.PNG", 30, 30);
 	}
 }
