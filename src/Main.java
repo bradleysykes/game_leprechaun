869d4ea1424212c96ead2a6ed59b188d5b8bd42b
Seunghyun Lee <shlee0605@gmail.com>
2013-11-17 21:30:39 -0500

    modified map decoder, added unit decoder

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import gae.EditGUI;
 
 public class Main {
 	public static void main(String[] args){
-		new EditGUI();
+	    new EditGUI();
 		//new GameViewer();
 		//ImageTool.scaleImage("C:\\Users\\Dylan\\cs308\\oogasalad_MISSINGNO\\testimages\\FFV_Barrier_Tower_SNES_BG.PNG", 30, 30);
 	}
---
+++ src/data/decoder/PlayerDecoder.java
@@ -0,0 +1,13 @@
+package data.decoder;
+
+import org.w3c.dom.Element;
+
+public class PlayerDecoder extends Decoder{
+    
+    @Override
+    public void decodeData (Element root) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
