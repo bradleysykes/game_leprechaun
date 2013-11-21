dc2ea7c5ba05ee6a82bede3d7e12a125483c0387
Dylan <dylanjaxxon@gmail.com>
2013-11-21 18:18:44 -0500

    image scaling utility complete

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,5 @@ public class Main {
 	public static void main(String[] args){
 	    new EditGUI();
 		//new GameViewer();
-		//ImageTool.scaleImage("C:\\Users\\Dylan\\cs308\\oogasalad_MISSINGNO\\testimages\\FFV_Barrier_Tower_SNES_BG.PNG", 30, 30);
 	}
 }
