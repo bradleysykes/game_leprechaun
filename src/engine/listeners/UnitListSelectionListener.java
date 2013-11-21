e27fca65feb3b7ec4bec6566f3d79673bed83711
Dylan <dylanjaxxon@gmail.com>
2013-11-21 00:01:09 -0500

    implemented UnitListSelectionListener

--- src/engine/listeners/UnitListSelectionListener.java
+++ src/engine/listeners/UnitListSelectionListener.java
@@ -1,9 +1,17 @@
 package engine.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JList;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import model.Abilities;
+import model.Ability;
+import model.Attributes;
+import model.stats.Stat;
+import model.stats.StatCollection;
 import model.unit.Unit;
 import engine.GameViewer;
 import engine.gui.AbilityListArea;
@@ -19,15 +27,42 @@ public class UnitListSelectionListener implements ListSelectionListener {
 	}
 
 	public void valueChanged(ListSelectionEvent e) {
-		JList list = (JList) e.getSource();
+		JList<String> list = (JList<String>) e.getSource();
 		int index = list.getSelectedIndex();
 		if (index != -1) {
 			Unit selectedUnit = myUnitListArea.getUnits().get(index);
-			//populate AbilityList, and UnitStatusArea
+			
 			AbilityListArea abilityListArea = (AbilityListArea) GameViewer.getActionPanel().getAbilityListArea();
 			UnitStatusArea unitStatusArea = (UnitStatusArea) GameViewer.getFeedbackPanel().getUnitStatusArea();
 			
+			StatCollection abilitiesStatCollection = selectedUnit.getStatCollection("Abilities");
+			populateAbilityListArea(abilitiesStatCollection, abilityListArea);
+			abilityListArea.setUnit(selectedUnit);
+			
+			StatCollection attributesStatCollection = selectedUnit.getStatCollection("Attributes");
+			setUnitStatusArea(attributesStatCollection, unitStatusArea, selectedUnit);
 		}
+	}
+	
+	private void populateAbilityListArea(StatCollection collection, AbilityListArea abilityListArea) {
+		Abilities abilities = (Abilities) collection;
+		List<Stat> abilityStatList = abilities.getStats();
+		List<Ability> abilityList = new ArrayList<Ability>();
+		for (Stat stat : abilityStatList) {
+			abilityList.add((Ability) stat);
+		}
+		
+		abilityListArea.refreshAbilities(abilityList);
+	}
+	
+	private void setUnitStatusArea(StatCollection collection, UnitStatusArea unitStatusArea, Unit unit) {
+		Attributes attributes = (Attributes) collection;
+		List<Stat> attributeStatList = attributes.getStats();
+		String attributeReport = unit.getID() + "\n";
+		for (Stat stat : attributeStatList) {
+			attributeReport += stat.getName() + ":" + " " + stat.getValue() + "\n";
+		}
+		unitStatusArea.setStatusText(attributeReport);
 	}
 
 }
