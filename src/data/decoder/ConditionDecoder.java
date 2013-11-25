c1c529696a929b81ef2c6c3044ce50f624906a64
Seunghyun Lee <shlee0605@gmail.com>
2013-11-25 02:07:50 -0500

    Condition Decoder now works

--- src/data/decoder/ConditionDecoder.java
+++ src/data/decoder/ConditionDecoder.java
@@ -3,8 +3,10 @@ package data.decoder;
 import java.util.ArrayList;
 import java.util.List;
 import model.Condition;
+import model.Player;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import util.reflection.Reflection;
 
 public class ConditionDecoder extends Decoder {
 
@@ -16,22 +18,22 @@ public class ConditionDecoder extends Decoder {
         myConditions = new ArrayList<Condition>();
     }
     
-    private processConditions(Element root) {
+    private void processConditions(Element root) {
         Element conditions = (Element)root.getElementsByTagName(CONDITIONS).item(0);
         NodeList conditionList = conditions.getElementsByTagName(CONDITION);
-
-        
-
-    }
-    
-    private Condition getCondition(Element condition) {
-        
-        
+        for (int i = 0; i < conditionList.getLength(); i ++) {
+            String className = ((Element) conditionList.item(i)).getAttribute(NAME);
+            String unitID = ((Element)conditionList.item(i)).getAttribute(UNIT_ID);
+            String playerID = ((Element)conditionList.item(i)).getAttribute(PLAYER_ID);
+            Player player = myDataManager.getPlayer(playerID);
+            myConditions.add((Condition)Reflection.createInstance(className, unitID, player));
+        }    
     }
     
     @Override
     public void decodeData (Element root) {
-        
+        processConditions(root);
+        myDataManager.setConditions(myConditions);
     }
 
 }
