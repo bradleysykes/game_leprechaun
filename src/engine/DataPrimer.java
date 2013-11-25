3998561467b6fb8bf123336aac60d6e9d926e080
JohnGodbey <jrg35@duke.edu>
2013-11-25 01:36:19 -0500

    not using the same test material

--- src/engine/DataPrimer.java
+++ src/engine/DataPrimer.java
@@ -21,7 +21,7 @@ public class DataPrimer {
 		myFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		myFileChooser.setFileFilter(filter);
 		File xmlFile = selectAndParseXML();*/
-		File xmlFile = new File("src/data/resources/savedTestFile.xml");		
+		File xmlFile = new File("src/data/resources/TestFile.xml");		
 		loadGame(xmlFile);
 	}
 	
