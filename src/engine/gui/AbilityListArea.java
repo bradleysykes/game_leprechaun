a108bb8df7faae29da2cb603efd40e3d13dec1d7
TSantala <timo.santala@duke.edu>
2013-11-25 01:36:51 -0500

    minor changes to engine

--- src/engine/gui/AbilityListArea.java
+++ src/engine/gui/AbilityListArea.java
@@ -21,7 +21,8 @@ public class AbilityListArea extends JPanel {
 	private GameEngine myEngine;
 	private Abilities myAbilities;
 	private JComboBox<String> myComboBoxModel;
-	
+	private boolean myStatus = false;
+
 	public AbilityListArea(GameEngine ge) {
 		myEngine = ge;
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
@@ -29,25 +30,31 @@ public class AbilityListArea extends JPanel {
 		myComboBoxModel.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println(myComboBoxModel.getSelectedItem().toString());
-				Ability ability = (Ability) myAbilities.getStat(myComboBoxModel.getSelectedItem().toString());
-				myEngine.getModel().setAbility(ability);
+				if(myStatus){
+					System.out.println(myComboBoxModel.getSelectedItem().toString());
+					Ability ability = (Ability) myAbilities.getStat(myComboBoxModel.getSelectedItem().toString());
+					myEngine.getModel().setAbility(ability);
+				}
+				else{		// Bug with first click. Should be a better solution.
+					myStatus = true;
+				}
 			}
 		});
 		this.add(new Label("Abilities:"));
 		this.add(myComboBoxModel);
 	}
-	
+
 	public void refreshAbilities(StatCollection a) {
 		myComboBoxModel.removeAllItems();
 		for (Stat s : a.getStats()) {
 			myComboBoxModel.addItem(s.getName());
 		}
+		myComboBoxModel.revalidate();
 	}
-	
+
 	public void setUnit(Unit unit) {
 		myAbilities = (Abilities) unit.getStatCollection("Abilities");
 		this.refreshAbilities(myAbilities);
 	}
-	
+
 }
