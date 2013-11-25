bf068ac29743d7d67859d892267d4394405d9536
Seunghyun Lee <shlee0605@gmail.com>
2013-11-25 01:24:07 -0500

    added condition decoder

---
+++ src/data/decoder/ConditionDecoder.java
@@ -0,0 +1,23 @@
+package data.decoder;
+
+import org.w3c.dom.Element;
+
+public class ConditionDecoder extends Decoder {
+
+    private DataManager myDataManager;
+    
+    public ConditionDecoder(DataManager manager) {
+        myDataManager = manager;
+    }
+    
+    private processConditions(Element root) {
+        
+    }
+    
+    @Override
+    public void decodeData (Element root) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
