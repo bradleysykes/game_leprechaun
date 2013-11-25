81831226aab01d9bbea177f2a943524c25d2237a
TSantala <timo.santala@duke.edu>
2013-11-25 02:01:16 -0500

    Fixed populating ability list

--- src/engine/TileHighlightObject.java
+++ src/engine/TileHighlightObject.java
@@ -10,16 +10,16 @@ public class TileHighlightObject extends JGObject {
 	private GameEngine myGameEngine;
 	private static final int myCollisionID = 4;
 	private final double myThickness = 3;
-	private final JGColor myColor = JGColor.black;
+	private final JGColor myColor = JGColor.cyan;
 	
 	public TileHighlightObject(Tile tile, GameEngine gameEngine) {
-		super("zhighlight", true, 0, 0, myCollisionID, null);
+		super("ahighlight", true, 0, 0, myCollisionID, null);
 		myTile = tile;   
 		myGameEngine = gameEngine;
 	}
 	
 	@Override
-	public void move() {
+	public void paint() {
 		myGameEngine.drawRect(myTile.getX()*myGameEngine.getGameTile(myTile).getImageBBox().width, 
 				myTile.getY()*myGameEngine.getGameTile(myTile).getImageBBox().height, 
 				myGameEngine.getGameTile(myTile).getImageBBox().width, 
