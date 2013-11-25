b2b2ac5df7a7f44092ab595ad6d565fea77498ba
alexsong93 <ss456@duke.edu>
2013-11-24 23:49:45 -0500

    modified xml file for testing player

--- src/data/encoder/EncoderMain.java
+++ src/data/encoder/EncoderMain.java
@@ -3,7 +3,6 @@ package data.encoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import model.Condition;
 import model.GameMap;
 import model.Player;
@@ -35,7 +34,7 @@ public class EncoderMain implements Elements {
         currentState.setGameMap(map);
         
         //add playerList to currentState
-/*        List<Player> playerList = new ArrayList<Player>();
+        List<Player> playerList = new ArrayList<Player>();
         Player p1 = new Player();
         p1.getStatCollection(RESOURCES).addStat(new Resource("minerals", 500, 0));
         p1.getStatCollection(RESOURCES).addStat(new Resource("gas", 350, 0));
@@ -51,7 +50,7 @@ public class EncoderMain implements Elements {
         p1.addUnit(unit2);
         
         playerList.add(p1); playerList.add(p2);
-        currentState.setPlayerList(playerList);*/
+        currentState.setPlayerList(playerList);
         
         //add Conditions to currentState
         List<Condition> conditionList = new ArrayList<Condition>();
@@ -60,9 +59,9 @@ public class EncoderMain implements Elements {
         currentState.setConditions(conditionList);
         
         //add imageMap to currentState
-        Map<String, String> unitImageMap = new HashMap<String, String>();
+        HashMap<String, String> unitImageMap = new HashMap<String, String>();
         unitImageMap.put("test1", "src/test1");
-        Map<String, String> tileImageMap = new HashMap<String, String>();
+        HashMap<String, String> tileImageMap = new HashMap<String, String>();
         tileImageMap.put("test1", "src/test2");
         //populate the imageMaps... then
         currentState.setUnitImageMap(unitImageMap);
