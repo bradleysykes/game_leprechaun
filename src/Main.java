08f4531cd3dc340ab80ca1eadcaddb715b13ab78
Seunghyun Lee <shlee0605@gmail.com>
2013-12-03 14:45:02 -0500

    resolved merge conflict

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import engine.GameViewer;
 import gae.EditGUI;
 
 public class Main {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
 	    new EditGUI();
 		//new GameViewer();
+
 	}
 }
