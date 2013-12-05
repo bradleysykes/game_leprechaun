037a04cc71ea8ffc84b4e469138b4d2486e55dc8
Seunghyun Lee <shlee0605@gmail.com>
2013-12-05 15:18:29 -0500

    merge conflict resolve, added relative path function to data loader

--- src/engine/DataPrimer.java
+++ src/engine/DataPrimer.java
@@ -27,7 +27,7 @@ public class DataPrimer {
 			xmlFile = gameEngine.getSourceFile();
 		}
 		else{
-			xmlFile = new File("src/test_saves/game3.xml");
+			xmlFile = new File("src/data/resources/testtest.xml");
 		}
 		System.out.println("Loading from file "+xmlFile.getAbsolutePath());
 		loadGame(xmlFile);
--- src/engine/GameLoader.java
+++ src/engine/GameLoader.java
@@ -1,10 +1,12 @@
 package engine;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import data.GameElements;
 import java.util.Collection;
 import java.util.Map;
-
 import model.GameMap;
 import model.Player;
 import model.tile.Tile;
@@ -21,7 +23,7 @@ public class GameLoader  {
 	}
 
 	public void loadGame() {
-		myGameEngine.removeObjects("-tile", GameTileObject.getCollisionID());
+		myGameEngine.removeObjects("tile", GameTileObject.getCollisionID());
 		myGameEngine.removeObjects("unit", GameUnitObject.getCollisionID());
 		GameMap gameMap = myGameElements.getGameMap();
 		myGameEngine.setPFSize(gameMap.getSizeX() , gameMap.getSizeY());
@@ -30,12 +32,12 @@ public class GameLoader  {
 		Map<String, String> myTileImages = myGameElements.getTileImageMap();
 		Map<String, String> myUnitImages = myGameElements.getUnitImageMap();
 
-		for (String all : myTileImages.keySet()) {
-			myGameEngine.defineImage(all, "-", 0,  "/" + myTileImages.get(all),"-");
+		for (String id : myTileImages.keySet()) {
+			myGameEngine.defineImage(id, "-", 0,  "/" + myTileImages.get(id),"-");
 		}		
 		
-		for (String all : myUnitImages.keySet()) {
-			myGameEngine.defineImage(all, "-", 0, "/" + myUnitImages.get(all),"-");
+		for (String id : myUnitImages.keySet()) {
+			myGameEngine.defineImage(id, "-", 0, "/" + myUnitImages.get(id),"-");
 		}
 
 		Collection<Player> allPlayers = myGameElements.getPlayers();
@@ -47,7 +49,20 @@ public class GameLoader  {
 		myGameEngine.setPlayers(allPlayers);
 		myGameEngine.initializeTiles(allTiles);
 		myGameEngine.initializeUnits(allUnits);
-
 	}
-
+	
+	/**
+	 * This method converts the abolute path into the relative path.
+	 * However, it will only work in linux or Mac OS because JGEngine
+	 * adds the string, "/", to the front.
+	 * 
+	 * @param path
+	 * @return
+	 */
+	private String getRelativePath(String path) {
+        String rootDir = System.getProperty("user.dir");
+        Path basePath = Paths.get(rootDir);
+        Path absolutePath = Paths.get(path);
+        return basePath.relativize(absolutePath).toString(); 
+	}
 }
