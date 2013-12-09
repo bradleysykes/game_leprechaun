7d8e95720b6e32f8f3421db2c769329c6aeb0d88
Seunghyun Lee <shlee0605@gmail.com>
2013-12-09 00:44:54 -0500

    custom ability creation tested

--- src/data/decoder/DataManager.java
+++ src/data/decoder/DataManager.java
@@ -105,7 +105,7 @@ public class DataManager extends GameElements implements Attributes, Elements {
     
    
     //test use
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
         DataManager dm = new DataManager();
         GameElements map = dm.getGameElements(new File("src/data/resources/TestFile.xml"));
         map.toString();
